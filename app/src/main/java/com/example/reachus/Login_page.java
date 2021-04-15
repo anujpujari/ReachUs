883b1e21f3e4a75280740d150de45d9d0b6f0a83
Sanket-777 <ksanket114402@gmail.com>
2021-04-15 20:39:48 +0530

    Added Login Using  Email-Sanket

--- app/src/main/java/com/example/reachus/Login_page.java
+++ app/src/main/java/com/example/reachus/Login_page.java
@@ -5,11 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -20,17 +26,25 @@ import com.google.firebase.database.ValueEventListener;
 import org.jetbrains.annotations.NotNull;
 
 public class Login_page extends AppCompatActivity {
- EditText number,pass;
- String s_number,s_pass;
+    private static final String TAG = "Email Password";
+    EditText email,password;
+ String s_email,s_pass;
  Button login,forgot_pass,signup;
-    DatabaseReference myref;
+    private FirebaseAuth mAuth ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
+        if(mAuth.getCurrentUser()!=null)
+        {
+            startActivity(new Intent(Login_page.this,MainActivity.class) );
+            finish();
+        }
         setContentView(R.layout.activity_login_page);
 
-        number = findViewById(R.id.l_phone_number);
-        pass = findViewById(R.id.l_password);
+        email = findViewById(R.id.l_email);
+        password = findViewById(R.id.l_password);
         login = findViewById(R.id.login_button);
         forgot_pass = findViewById(R.id.forgotpasword_button);
         signup = findViewById(R.id.Sign_up_button);
@@ -42,43 +56,81 @@ public class Login_page extends AppCompatActivity {
                 //Redirects the user to the registration Page
                 Intent i = new Intent(Login_page.this,registration_page.class);
                 startActivity(i);
+                finish();
             }
         });
 
-        s_number = number.getText().toString();
-        s_pass = pass.getText().toString();
+        s_email = email.getText().toString();
+        s_pass = password.getText().toString();
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                myref    = FirebaseDatabase.getInstance().getReference().child("User Details");
 
-                myref.orderByChild("Phone_number").equalTo(s_number).addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        if(snapshot.exists()){
-                            myref.orderByChild("Password").equalTo(s_pass).addValueEventListener(new ValueEventListener() {
-                                @Override
-                                public void onDataChange(@NonNull DataSnapshot snapshot) {
-                                    Intent i = new Intent(Login_page.this,MainActivity.class);
-                                }
-
-                                @Override
-                                public void onCancelled(@NonNull DatabaseError error) {
-
-                                }
-                            });
-                            //username exist
-                        }
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-
-                    }
-                });
-
-
+                signIn();
             }
         });
     }
+
+    private void signIn() {
+
+        Log.d(TAG,"signin"+email);
+        if (!validateform())
+        {
+            return;
+        }
+
+        String em = email.getText().toString();
+        String pw = password.getText().toString();
+
+
+        mAuth.signInWithEmailAndPassword(em,pw)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful())
+                        {
+                            Toast.makeText(Login_page.this,"Sign-In Sucessfull",Toast.LENGTH_SHORT).show();
+                            Intent i = new Intent(Login_page.this,MainActivity.class);
+                            startActivity(i);
+                            finish();
+                        }
+                        else
+                        {
+                            Log.w(TAG,"Failed to Sign-in",task.getException());
+                            Toast.makeText(Login_page.this,task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
+
+
+    private boolean validateform() {
+        boolean valid = true;
+        String em = email.getText().toString();
+        if(TextUtils.isEmpty(em)){
+            email.setError("Field Empty");
+            valid = false;
+        }
+
+        else
+        {
+            email.setError(null);
+        }
+
+        String pw = password.getText().toString();
+        if(TextUtils.isEmpty(pw)){
+            password.setError("Field Empty");
+            valid = false;
+        }
+        else
+        {
+            password.setError(null);
+        }
+        return valid;
+
+    }
+
+
 }
