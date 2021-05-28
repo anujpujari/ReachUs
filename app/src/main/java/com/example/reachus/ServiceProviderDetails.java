76dd9fdc519978d89a543428fa53aaff1c153515
Sanket-777 <ksanket114402@gmail.com>
2021-05-28 11:16:52 +0530

    Added Regions

--- app/src/main/java/com/example/reachus/ServiceProviderDetails.java
+++ app/src/main/java/com/example/reachus/ServiceProviderDetails.java
@@ -2,17 +2,18 @@ package com.example.reachus;
 //this class stores these information to the Database
 public class
 ServiceProviderDetails {
-    public String s_number,s_age,s_gender,s_name,s_about,s_service;
+    public String s_number,s_age,s_gender,s_name,s_about,s_service,s_region;
     public ServiceProviderDetails() {
     }
 
 
-    public ServiceProviderDetails(String s_number, String s_age, String s_gender, String s_name, String s_about,String s_service) {
+    public ServiceProviderDetails(String s_number, String s_age, String s_gender, String s_name, String s_about,String s_service,String s_region) {
         this.s_number = s_number;
         this.s_about = s_about;
         this.s_name = s_name;
         this.s_gender = s_gender;
         this.s_age = s_age;
         this.s_service= s_service;
+        this.s_region=s_region;
     }
 }
--- app/src/main/java/com/example/reachus/Service_Provider_Info.java
+++ app/src/main/java/com/example/reachus/Service_Provider_Info.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,22 +23,24 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Service_Provider_Info extends AppCompatActivity {
-    Spinner myspinner;
+    Spinner myspinner,myspinner2;
     EditText Phone_number,Age,about,name;
     RadioButton mal,femal;
     RadioGroup Gen;
-    String s_number,s_age,s_gender,s_about,s_name,s_service;
+    String s_number,s_age,s_gender,s_about,s_name,s_service,s_region;
     Button submit;
     String MobilePattern = "[0-9]{10}";
     private static final String TAG = "EmailPassword";
     private FirebaseAuth mAuth;
     FirebaseFirestore fStore;
     String userId;
+    boolean valid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service__provider__info);
 
+        myspinner2 = findViewById(R.id.sp_region);
         name  = findViewById(R.id.sp_Username);
         myspinner = findViewById(R.id.u_service);
         submit = findViewById(R.id.sp_submit1);
@@ -48,22 +51,37 @@ public class Service_Provider_Info extends AppCompatActivity {
         about = findViewById(R.id.aboutsp);
         mAuth=FirebaseAuth.getInstance();
         userId = mAuth.getCurrentUser().getUid();
-        SharedPreferences mySharedPreferences = this.getSharedPreferences("MYPREFERENCENAME", Context.MODE_PRIVATE);
 
 
 
 
         Gen = findViewById(R.id.sp_gender);
         ArrayAdapter<String> myadapter = new ArrayAdapter<>(Service_Provider_Info.this, android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.Services));
+        ArrayAdapter<String> myadapter2 = new ArrayAdapter<>(Service_Provider_Info.this, android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.Region));
+        myadapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         myadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        myspinner2.setAdapter(myadapter2);
         myspinner.setAdapter(myadapter);
          s_service = myspinner.getSelectedItem().toString();
+        s_region = myspinner2.getSelectedItem().toString();
         s_name = name.getText().toString();
         s_number = Phone_number.getText().toString();
         s_age = Age.getText().toString();
         s_about = about.getText().toString();
 
 
+        myspinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Toast.makeText(getApplicationContext(),"Select the Service Please",Toast.LENGTH_SHORT).show();
+                valid = false;
+            }
+        });
 
         ///////////////////////////////////////////////////////
         submit.setOnClickListener(new View.OnClickListener() {
@@ -101,30 +119,30 @@ public class Service_Provider_Info extends AppCompatActivity {
             FirebaseDatabase database = FirebaseDatabase.getInstance();
             DatabaseReference myref = database.getReference("Service Provider Details");
             ServiceProviderDetails val;
-            val = new ServiceProviderDetails(s_number,s_age,s_gender,s_name,s_about,s_service);
+            val = new ServiceProviderDetails(s_number,s_age,s_gender,s_name,s_about,s_service,s_region);
             myref.child(userId).setValue(val);
             if(s_service.equals("Car Service"))
             {
                  myref = database.getReference("Car Service Provider Details");
-                val = new ServiceProviderDetails(s_number,s_age,s_gender,s_name,s_about,s_service);
+                val = new ServiceProviderDetails(s_number,s_age,s_gender,s_name,s_about,s_service,s_region);
                 myref.child(userId).setValue(val);
             }
             else if (s_service=="Repairing Service")
             {
                  myref = database.getReference("Repairing Service Provider Details");
-                val = new ServiceProviderDetails(s_number,s_age,s_gender,s_name,s_about,s_service);
+                val = new ServiceProviderDetails(s_number,s_age,s_gender,s_name,s_about,s_service,s_region);
                 myref.child(s_service).setValue(val);
             }
             else if (s_service=="Maid Service")
             {
                  myref = database.getReference("Maid Service Provider Details");
-                val = new ServiceProviderDetails(s_number,s_age,s_gender,s_name,s_about,s_service);
+                val = new ServiceProviderDetails(s_number,s_age,s_gender,s_name,s_about,s_service,s_region);
                 myref.child(s_service).setValue(val);
             }
             if (s_service=="Cleaning Service")
             {
               myref = database.getReference("Cleaning Service Provider Details");
-                val = new ServiceProviderDetails(s_number,s_age,s_gender,s_name,s_about,s_service);
+                val = new ServiceProviderDetails(s_number,s_age,s_gender,s_name,s_about,s_service,s_region);
                 myref.child(s_service).setValue(val);
             }
         }else{
@@ -133,7 +151,7 @@ public class Service_Provider_Info extends AppCompatActivity {
 }
 
     public boolean validateform() {
-        boolean valid = true;
+         valid = true;
         s_name = name.getText().toString();
         if (TextUtils.isEmpty(s_name)) {
             name.setError("Field Empty");
